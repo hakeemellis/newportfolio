e8126799ba08818bb0660b92b63e0545129885ed
Hakeem Ellis <114674803+hakeemellis@users.noreply.github.com>
2024-12-27 15:40:39 -0500

    SVGs added with prettier setup

--- portfolio/src/components/Default.vue
+++ portfolio/src/components/Default.vue
@@ -1,30 +1,30 @@
 <template>
-    <div>
-      <h1>Component Title</h1>
-      <p>Component description or content goes here.</p>
-    </div>
-  </template>
-  
-  <script>
+  <div>
+    <h1>Component Title</h1>
+    <p>Component description or content goes here.</p>
+  </div>
+</template>
+
+<script>
   export default {
     name: 'DefaultComponent',
     data() {
       return {
-        message: 'This is a default reusable Vue component!'
+        message: 'This is a default reusable Vue component!',
       };
-    }
+    },
   };
-  </script>
-  
-  <style scoped>
+</script>
+
+<style scoped>
   /* Default styles for the component */
   h1 {
     color: blue;
     font-size: 2rem;
   }
-  
+
   p {
     color: gray;
     font-size: 1rem;
   }
-  </style>
+</style>
