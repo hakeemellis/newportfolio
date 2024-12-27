eac0509ad37f9706097ee5feb9c9646e3407a045
Hakeem Ellis <114674803+hakeemellis@users.noreply.github.com>
2024-12-26 21:45:04 -0500

    Added a default vue file to help keep track

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,18 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Default from './components/Default.vue';
+import StagFrame from './components/StagFrame.vue';
 </script>
 
 <template>
     <section>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://vuejs.org/" target="_blank">
-          <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-        </a>
-      </div>
-      <HelloWorld />
+      <StagFrame />
   </section>
 </template>
 
