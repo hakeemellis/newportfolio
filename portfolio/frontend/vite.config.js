b36a8de91b828d3055dcab6c55c01edfbf466a66
Hakeem Ellis <114674803+hakeemellis@users.noreply.github.com>
2025-02-09 18:33:34 -0500

    Enhance Vite configuration with server settings and API proxy

--- portfolio/frontend/vite.config.js
+++ portfolio/frontend/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// https://vitejs.dev/config/
 
-// https://vite.dev/config/
+// Congiguration for Vite
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [vue()], // Vue plugin
+  // Server configuration
+  server: {
+    port: parseInt(process.env.VITE_PORT), // Port for frontend
+    host: '0.0.0.0', // To allow network access
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_BASE_URL, // Backend URL
+        changeOrigin: true, // To allow network access to backend through CORS
+        rewrite: (path) => path.replace(/^\/api/, ''), // Regex to remove '/api' prefix
+      },
+    },
+  },
+});
