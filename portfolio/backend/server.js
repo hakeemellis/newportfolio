20d8713aacf0915a6bc43774485b9b0da6a89835
Hakeem Ellis <114674803+hakeemellis@users.noreply.github.com>
2025-02-14 15:10:07 -0500

    Add new session ID generation using crypto

--- portfolio/backend/server.js
+++ portfolio/backend/server.js
@@ -26,6 +26,7 @@ const cors = require("cors"); // cors for handling cross-origin requests
 const http = require("http"); // http for handling WebSockets
 const session = require("express-session"); // to assist with session management (for authentication and security) i.e. server-side security
 const MongoStore = require("connect-mongo");
+const crypto = require("crypto");
 
 // Import Modular Variables: Database and Routes
 const app = express(); // defining the variable "app" to allow express to establish routing
@@ -68,13 +69,7 @@ app.use(
     saveUninitialized: false, // Prevents unneccessary sessions from being stored to database - for performance eg. if true, and user only visits the site an empty session will be created within my database and stored
     // "false" is better for performance and security - because it forces the user to be authenticated before a session is created
     store: store,
-    genid: (req) => {
-      return req.user
-        ? req.session.user
-          ? req.session.user
-          : req.user.id // If authenticated, use session user or user ID
-        : (Math.random() * 1e6).toString(); // Fallback to random ID if not authenticated
-    },
+    genid: () => crypto.randomUUID(), // Always generate a new random unique ID for the session ID
     cookie: {
       secure: process.env.NODE_ENV === "production", // Set to true if using HTTPS - for security
       httpOnly: true, // Prevents client-side JavaScript from accessing the cookie - for security
